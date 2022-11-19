82936318597c4a9bdc8b2e3d562de9e98f5f4d7c
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-11-19 13:09:24 -0500

    Adding styles to REST-Rant

--- views/home.jsx
+++ views/home.jsx
@@ -6,10 +6,14 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <a href="/places">
+                    <button className="btn btn-primary">Places Page</button>
+                </a>
             </main>
         </Def>
     )
 }
 
 module.exports = home
+
 
