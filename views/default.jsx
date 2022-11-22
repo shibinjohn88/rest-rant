7320adf93181c356b47e8c009eb515c0d914699c
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-11-21 19:25:52 -0500

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,20 @@
                 <link rel='stylesheet' href='/style.css'></link>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                        <a href="/">Home</a>
+                        </li>
+                        <li>
+                        <a href="/places">Places</a>
+                        </li>
+                        <li>
+                        <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
+
                 {html.children}
             </body>
         </html>
