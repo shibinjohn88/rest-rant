5c5a12c692b617e05abe3c305193c48d3e4a54ee
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-11-21 20:35:22 -0500

    style and content to show

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,31 @@
 const React = require('react')
+const places = require('../../models/places')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='imageholder col'>
+                            <img src={data.place.pic} class="img-fluid" alt="..."/>
+                        </div>
+                        <div className='placedescription col'>
+                            <h1>{data.place.name}</h1>
+                            <h2>Rating</h2>
+                            <p>Not rated</p>
+                            <h2>Description</h2>
+                            <p>Located in {data.place.city},{data.place.state} and serving {data.place.cuisines}</p>
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <h2>Comments</h2>
+                        <p>No comments yet!</p>
+                    </div>
+                </div>
+                
             </main>
         </Def>
     )
