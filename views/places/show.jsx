9275c5da5d1908eac7d4668dcc3807061431f4d0
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-12-08 20:48:50 -0500

    rating display using stars

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -18,9 +18,13 @@ function show (data) {
             return tot + c.stars
         }, 0)
         let averageRating = sumRatings / data.place.comments.length
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+            stars += '⭐'
+        } 
         rating = (
             <h3>
-                { Math.round(averageRating) } stars
+                { stars } stars
             </h3>
         )
         comments = data.place.comments.map( c => {
