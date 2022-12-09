475c5dc18b3ab084bb3576a8bd79aa213abe2058
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-12-08 20:42:46 -0500

    calculate average place rating

--- controllers/places.js
+++ controllers/places.js
@@ -44,7 +44,7 @@ router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
     .populate('comments')
     .then (place => {
-      console.log(place.comments)
+      // console.log(place.comments)
       res.render('places/show', { place })
     })
     .catch (err => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,10 +8,24 @@ function show (data) {
             No comments yet ! 
         </h3>
     )
+    let rating = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce( (tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                { Math.round(averageRating) } stars
+            </h3>
+        )
         comments = data.place.comments.map( c => {
             return (
-                <div className='border col-sm-6'>
+                <div className='border col-sm-6' key={c.id}>
                     <h2 className='rant'>{c.rant ? 'Rant!' : 'Rave!'}</h2>
                     <h4>{ c.content }</h4>
                     <h3>
@@ -36,7 +50,7 @@ function show (data) {
                     <div className='col-sm-6'>
                         <h1>{data.place.name}</h1>
                         <h2>Rating</h2>
-                        <p>Not rated</p>
+                        { rating }
                         <h2>Description</h2>
                         <h3>
                             {data.place.showEstablished()}
